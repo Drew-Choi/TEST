e03d5dbdd7bfb1e80fe31f8e84527d8869c5af93
Drew <drc920630@gmail.com>
2023-02-11 16:09:15 +0900

    아 이해 다 했다!!! 이제 정규식으로다시 풀어보기

--- WantedTestByteCut.js
+++ WantedTestByteCut.js
@@ -5,34 +5,26 @@
 let str = "이 글은 도커에 대해 1도 모르는 시스템 관리자나 서버 개발자를 대상으로 도커 전반에 대해 얕고 넓은 지식을 담고 있습니다. 도커가 등장한 배경과 도커의 역사, 그리고 도커의 핵심 개념인 컨테이너와 이미지에 대해 알아보고 실제로 도커를 설치하고 컨테이너를 실행해 보도록 하겠습니다.";
 
 
-
-
 // 위의 문장을 80byte 기준으로 잘라서 배열에 담기
 // 한글은 2byte, 한글을 제외한 영어, 숫자, 기호, 띄어쓰기는 1byte
 // 자르고 난 다음 글자가 공백일 경우는 생략처리
 
 let BYTE = 0;
-let deCoding = "";
 let Result = [];
-let totalLen = 0;
-let pre = 0;
+let preTextlen = 0;
 
 for (let i = 0; i < str.length; i += 1) {
-     console.log(i)
     let Char = escape(str.charAt(i));
-
     Char.length > 4 ? BYTE += 2 : BYTE += 1;
-
+    console.log(BYTE);
     if (BYTE >= 80 ) {
-     
-      Result.push(str.substring(pre, i).trim());
-      pre += i - pre;
-     
-      
-
-     BYTE = 0;
+      console.log(i);
+      Result.push(str.slice(preTextlen, i).trim());
+      preTextlen += i - preTextlen;     
+      BYTE = 0;
     }
   }
 
-  console.log(Result);
-  
+  Result.push(str.slice(preTextlen, str.length));
+
+  console.log(Result)
