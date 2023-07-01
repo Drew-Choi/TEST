1689f51873f9005e014fcb74b5e8692230a58631
Drew <drc920630@gmail.com>
2023-07-01 19:44:57 +0900

    g플래그에 대해

--- 숫자찾아합.js
+++ 숫자찾아합.js
@@ -2,12 +2,14 @@ function solution(my_string) {
   const regex = /^[0-9]+$/
   
   const answer = Array.from(my_string).reduce((acc, cur) => {if(regex.test(cur)){
-      console.log(cur); return acc + Number(cur)
+      console.log('숫자인식', cur); return acc + Number(cur)
   }else {
-     console.log(cur); return acc
+     console.log('테스트못한거', cur); return acc
   }}, 0 )
   
   return answer;
 }
 
 console.log(solution('abc123'));
+
+// g플래그 사용에 유의하라
